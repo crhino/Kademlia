08105220ca4bd70b2373dba0ab61fc6b0e4a22d5
crhino <piraino.chris@gmail.com>
2013-11-22 15:35:53 -0500

    Setting up testing of Entangled Nodes.

--- sybil_attack.py
+++ sybil_attack.py
@@ -14,30 +14,47 @@ nodes = []
 # The Sybil attack nodes, cooperating to comprimise network.
 sybils = []
 
+# List of known Entangled nodes
+knownNodes = []
+
+# Starting port number
+port = 4000
+
 # The key to use for this example when storing/retrieving data
 KEY = 'example_key'
 # The value to store
 VALUE = 'example_value'
 
-def action():
-    i=0
+def print_info():
     print len(nodes)
     for node in nodes:
         node.printContacts()
-    time.sleep(2)
 
 def newNode():
-  global port
-  dataStore = SQLiteDataStore(dbFile = '/tmp/dbFile%s.db' % port)
-  node = EntangledNode (udpPort=port, dataStore=dataStore)
-  node.joinNetwork(knownNodes)
-  nodes.append(node)
-  knownNodes.append(('127.0.0.1', port))
-  port+=1
+    global port
+
+    print 'Creating Entangled Node...'
+    if os.path.isfile('/tmp/dbFile%s.db' % port):
+        os.remove('/tmp/dbFile%s.db' % port)
+    dataStore = SQLiteDataStore(dbFile = '/tmp/dbFile%s.db' % port)
+    node = EntangledNode (udpPort=port, dataStore=dataStore)
+    node.joinNetwork(knownNodes)
+    nodes.append(node)
+    print node.ping()
+    knownNodes.append(('127.0.0.1', port))
+    port+=1
 
 
 def mainLoop():
-  
+    uinput = ''
+    while uinput != 'quit':
+        uinput = raw_input("> ")
+        if uinput == 'new':
+          newNode()
+        elif uinput == 'print':
+          print_info()
+    stop()
+
 
 def stop():
     """ Stops the Twisted reactor, and thus the script """
@@ -47,8 +64,6 @@ def stop():
 if __name__ == '__main__':
     
     import sys, os
-    port = 4000 # Starting UDP port for Kad nodes.
-    knownNodes = []
 
     if len(sys.argv) != 2:
         print 'Usage:\n%s [NUM_NODES]' % sys.argv[0]
@@ -61,12 +76,8 @@ if __name__ == '__main__':
 
 
     for x in range(0, int(sys.argv[1])):
-        if os.path.isfile('/tmp/dbFile%s.db' % port):
-            os.remove('/tmp/dbFile%s.db' % port)
-        print 'Creating Entangled Node...'
         newNode()    
     twisted.internet.reactor.callLater(2.5, mainLoop)
     # Start the Twisted reactor
     print 'Twisted reactor started (script will commence in 2.5 seconds)'
     twisted.internet.reactor.run()
-
