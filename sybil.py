de12c0b7aa344d8be4a863bd156efcd9709c18ca
crhino <piraino.chris@gmail.com>
2013-12-03 22:08:15 -0500

    Sybil Node is correctly returning a different value than stored.

---
+++ sybil.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+# The Sybil class for a Kademila node.
+# Chris Piraino
+
+from twisted.internet import defer
+
+from entangled.node import EntangledNode
+
+def rpcmethod(func):
+    func.rpcmethod = True
+    return func
+
+class SybilNode(EntangledNode):
+    """ An Entangled node that shows how a sybil attack can be implemented
+    and the different attack vectors that can be taken. """
+
+    def __init__(self, id=None, udpPort=5000, dataStore=None, routingTable=None, networkProtocol=None):
+        EntangledNode.__init__(self, id, udpPort, dataStore, routingTable, networkProtocol)
+
+#    @rpcmethod
+#    def findValue(self, key, **kwargs):
+#        print "key: %s\n" % key
+#        if key in self._dataStore:
+#            return {key: "payload"}
+#        else:
+#            return self.findNode(key, **kwargs)
+
+
+    def iterativeFindValue(self, key):
+        """ The Kademlia search operation (deterministic)
+        
+        Call this to retrieve data from the DHT.
+        
+        @param key: the 160-bit key (i.e. the value ID) to search for
+        @type key: str
+        
+        @return: This immediately returns a deferred object, which will return
+                 either one of two things:
+                     - If the value was found, it will return a Python
+                     dictionary containing the searched-for key (the C{key}
+                     parameter passed to this method), and its associated
+                     value, in the format:
+                     C{<str>key: <str>data_value}
+                     - If the value was not found, it will return a list of k
+                     "closest" contacts (C{kademlia.contact.Contact} objects)
+                     to the specified key
+        @rtype: twisted.internet.defer.Deferred
+        """
+        print 'Sybil Node'
+        # Prepare a callback for this operation
+        outerDf = defer.Deferred()
+        def checkResult(result):
+            if type(result) == dict:
+                # We have found the value; now see who was the closest contact without it...
+                if 'closestNodeNoValue' in result:
+                    # ...and store the key/value pair
+                    contact = result['closestNodeNoValue']
+                    contact.store(key, result[key])
+                outerDf.callback(result)
+            else:
+                # The value wasn't found, but a list of contacts was returned
+                # Now, see if we have the value (it might seem wasteful to search on the network
+                # first, but it ensures that all values are properly propagated through the
+                # network
+                if key in self._dataStore:
+                    # We have the key stored, but we aren't nice and give an arbitrary payload.
+                    value = 'payload'
+                    # Send this payload to other nodes to really mess things up.
+                    if len(result) > 0:
+                        contact = result[0]
+                        contact.store(key, value)
+                    outerDf.callback({key: value})
+                else:
+                    # Ok, value does not exist in DHT at all
+                    outerDf.callback(result)
+
+        # Execute the search
+        df = self._iterativeFind(key, rpc='findValue')
+        df.addCallback(checkResult)
+        return outerDf
+
+
--- sybil_attack.py
+++ sybil_attack.py
@@ -7,18 +7,14 @@ import os, sys, time, signal
 import twisted.internet.reactor
 from entangled.node import EntangledNode
 from entangled.kademlia.datastore import SQLiteDataStore
+from sybil import SybilNode
 
-# The Entangled DHT nodes; instantiated in the main() method
-nodes = []
+# The Entangled DHT Sybil node; instantiated in the newNode() method
+sybil = None
 
-# The Sybil attack nodes, cooperating to comprimise network.
-sybils = []
-
-# List of known Entangled nodes
-knownNodes = []
 
 # Starting port number
-port = 4000
+port = 5000
 
 # The key to use for this example when storing/retrieving data
 KEY = 'example_key'
@@ -26,31 +22,82 @@ KEY = 'example_key'
 VALUE = 'example_value'
 
 def print_info():
-    print len(nodes)
-    for node in nodes:
-        node.printContacts()
+    sybil.printContacts()
+    print '\n\n'
 
-def newNode():
+
+# Function: newNode
+# Args: node_list - list of knownNodes
+#
+# Creates a sybil node.
+def newNode(node_list):
     global port
-
+    global sybil
     print 'Creating Entangled Node...'
     if os.path.isfile('/tmp/dbFile%s.db' % port):
         os.remove('/tmp/dbFile%s.db' % port)
     dataStore = SQLiteDataStore(dbFile = '/tmp/dbFile%s.db' % port)
-    node = EntangledNode (udpPort=port, dataStore=dataStore)
-    node.joinNetwork(knownNodes)
-    nodes.append(node)
-    print node.ping()
-    knownNodes.append(('127.0.0.1', port))
-    port+=1
+    sybil = SybilNode (id=KEY, udpPort=port, dataStore=dataStore)
+    sybil.joinNetwork(node_list)
+    print sybil
+
+
+# Taken from entangled examples.
+#
+# A generic error callback.
+def genericErrorCallback(failure):
+    """ Callback function that is invoked if an error occurs during any of the DHT operations """
+    print 'An error has occurred:', failure.getErrorMessage()
+    twisted.internet.reactor.callLater(0, stop)
+
+# Taken from entangled examples.
+#
+# Does a findVal RPC on the DHT for specified key.
+def getValue(key):
+    """ Retrieves the value of the specified key (KEY) from the DHT """
+    global sybil
+    # Get the value for the specified key (immediately returns a Twisted deferred result)
+    print '\nRetrieving value from DHT for key "%s"...' % key
+    deferredResult = sybil.iterativeFindValue(key)
+    # Add a callback to this result; this will be called as soon as the operation has completed
+    deferredResult.addCallback(getValueCallback)
+    # add the generic error callback
+    deferredResult.addErrback(genericErrorCallback)
+
+# Taken from entangled examples.
+#
+# The findVal RPC passed result to us.
+def getValueCallback(result):
+    """ Callback function that is invoked when the getValue() operation succeeds """
+    # Check if the key was found (result is a dict of format {key: value}) or not (in which case a list of "closest" Kademlia contacts would be returned instead")
+    print result
+    if type(result) == dict:
+        print 'Value successfully retrieved: %s' % result[KEY]
+    else:
+        print 'Value not found'
+
+# Function: storeVal
+# Args: key, val - Both strings.
+#
+# Stores val in the DHT using the given key.
+def storeVal(key, val):
+    print "Storing (key,val) pair: (\"%s\", \"%s\")." % (key, val)
+    deferredResult = sybil.iterativeStore(key, val)
+    deferredResult.addCallback(storeValCallback)
+    deferredResult.addErrback(genericErrorCallback)
+
+def storeValCallback(*args, **kwargs):
+    print 'Value has been stored in the DHT'
 
 
 def mainLoop():
     uinput = ''
     while uinput != 'quit':
         uinput = raw_input("> ")
-        if uinput == 'new':
-          newNode()
+        if uinput == 'store':
+          storeVal(KEY, VALUE)
+        elif uinput == 'get':
+          getValue(KEY)
         elif uinput == 'print':
           print_info()
     stop()
@@ -66,17 +113,17 @@ if __name__ == '__main__':
     import sys, os
 
     if len(sys.argv) != 2:
-        print 'Usage:\n%s [NUM_NODES]' % sys.argv[0]
+        print 'Usage:\n%s [KNOWN_UDP_PORT]' % sys.argv[0]
 
     try:
         int(sys.argv[1])
     except ValueError:
-        print '\nNUM_NODES must be an integer value.\n'
+        print '\nNUM_REG_NODES must be an integer value.\n'
         sys.exit(1)
-
-
-    for x in range(0, int(sys.argv[1])):
-        newNode()    
+    
+    
+    newNode([('127.0.0.1', int(sys.argv[1]))])    
+    
     twisted.internet.reactor.callLater(2.5, mainLoop)
     # Start the Twisted reactor
     print 'Twisted reactor started (script will commence in 2.5 seconds)'
